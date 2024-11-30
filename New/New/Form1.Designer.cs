cb4462da4376ceee428120ca9802de9734d072c3
9ducanh9 <lamchitai2200@gmail.com>
2024-11-30 23:17:51 +0700

    Add files via upload

---
+++ New/New/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace New
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtaStudent = new System.Windows.Forms.DataGridView();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnFix = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtaStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtaStudent
+            // 
+            this.dtaStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtaStudent.Location = new System.Drawing.Point(37, 37);
+            this.dtaStudent.Name = "dtaStudent";
+            this.dtaStudent.RowHeadersWidth = 51;
+            this.dtaStudent.RowTemplate.Height = 24;
+            this.dtaStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtaStudent.Size = new System.Drawing.Size(416, 267);
+            this.dtaStudent.TabIndex = 0;
+            this.dtaStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(521, 65);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(210, 22);
+            this.txtMa.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(521, 152);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(210, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(521, 243);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(210, 22);
+            this.txtAge.TabIndex = 3;
+            this.txtAge.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnFix
+            // 
+            this.btnFix.Location = new System.Drawing.Point(351, 373);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(87, 37);
+            this.btnFix.TabIndex = 4;
+            this.btnFix.Text = "Sửa";
+            this.btnFix.UseVisualStyleBackColor = true;
+            this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(575, 373);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(93, 37);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Xoá";
+            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(129, 373);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(86, 37);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btn_Add);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnFix);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.dtaStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtaStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtaStudent;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ New/New/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using New;
+
+namespace New
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Student> students;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Khởi tạo danh sách học sinh ban đầu
+            students = new List<Student>();
+            students.Add(new Student() { ID = 1, Name = "A", Age = 20 });
+            students.Add(new Student() { ID = 2, Name = "B", Age = 21 });
+            students.Add(new Student() { ID = 3, Name = "C", Age = 22 });
+            students.Add(new Student() { ID = 4, Name = "D", Age = 23 });
+            dtaStudent.DataSource = students;
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dtaStudent.DataSource = null;
+            dtaStudent.DataSource = students;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dtaStudent.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a student to delete.");
+                return;
+            }
+
+            // Lấy học sinh được chọn và xóa
+            var selectedStudent = students[dtaStudent.SelectedRows[0].Index];
+            students.Remove(selectedStudent);
+
+            // Cập nhật lại DataGridView hoặc ListBox
+            dtaStudent.DataSource = new BindingList<Student>(students);
+        }
+
+        private void btn_Add(object sender, EventArgs e)
+        {
+            try
+            {
+                int studentID = int.Parse(txtMa.Text);
+                string name = txtName.Text;
+                int age = int.Parse(txtAge.Text);
+
+                if (students.Any(s => s.ID == studentID))
+                {
+                    MessageBox.Show("Mã sinh viên đã tồn tại!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                students.Add(new Student
+                {
+                    ID = studentID,
+                    Name = name,
+                    Age = age
+                });
+
+                
+                MessageBox.Show("Thêm sinh viên thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnFix_Click(object sender, EventArgs e)
+        {
+            if (dtaStudent.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a student to edit.");
+                return;
+            }
+
+            // Lấy học sinh được chọn
+            var selectedStudent = students[dtaStudent.SelectedRows[0].Index];
+            selectedStudent.ID = int.Parse(txtMa.Text);
+            selectedStudent.Name = txtName.Text;
+            selectedStudent.Age = int.Parse(txtAge.Text);
+
+            // Cập nhật lại DataGridView hoặc ListBox
+            dtaStudent.DataSource = new BindingList<Student>(students);
+        }
+    }
+}
+
+
